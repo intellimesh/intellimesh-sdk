a87dd4010d36bcf9867581764a9788ab9a7b52d1
rampenke <rampenke@gmail.com>
2024-04-13 22:45:56 -0700

    cortex added

---
+++ cortex/node/src/main.rs
@@ -0,0 +1,14 @@
+//! Substrate Parachain Node Template CLI
+
+#![warn(missing_docs)]
+
+mod chain_spec;
+mod cli;
+mod command;
+mod rpc;
+mod service;
+
+fn main() -> sc_cli::Result<()> {
+	println!("Starting Cortex");
+	command::run()
+}
